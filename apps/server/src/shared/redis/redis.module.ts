f53c5d21faf31b1b0938f6e35b3609cd015a3f03
Alexahder Kochish <shvepsolek@gmail.com>
2025-06-09 12:30:31 +0100

    fix: redis tls

--- apps/server/src/shared/redis/redis.module.ts
+++ apps/server/src/shared/redis/redis.module.ts
@@ -26,7 +26,7 @@ import { RedisService } from './redis.service';
           host: process.env.REDIS_HOST || 'localhost',
           port: Number(process.env.REDIS_PORT) || 6379,
           password: process.env.REDIS_PASSWORD || undefined,
-          // tls: {},
+          tls: {},
           retryStrategy(times) {
             return Math.min(times * 50, 2000);
           },
